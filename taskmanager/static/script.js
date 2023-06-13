43f063e1a3f9445185f1036f95e2bf96ce260dfd
helenahagman <>
2023-06-13 08:40:04 +0000

    add static folder

---
+++ taskmanager/static/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // sidenav initialization
+    var sidenam = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
